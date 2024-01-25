63395ac77966dd193482079e862708c470625f00
Querem Nunes <149440046+QueremNunes@users.noreply.github.com>
2024-01-25 11:07:11 -0300

    alteração

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function carregar() {
     var img = document.getElementById('imagem')
     var data = new Date() 
     var hora = data.getHours()
-    msg.innerHTML = `Agora são ${hora} horas.`
+    msg.innerHTML = `Agora são: ${hora} horas.`
     if (hora >= 0 && hora < 12) {
         img.src = 'manha.jpg'
         document.body.style.background = '#fdbe90'
